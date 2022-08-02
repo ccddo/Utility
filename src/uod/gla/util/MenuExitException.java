475eed8627b6bc0d64b0c6aa9d5551ef3ea95ddf
Chi <Chi@Home-PC.Home>
2022-08-02 18:41:38 +0100

    Added new exception type to represent menu exits

---
+++ src/uod/gla/util/MenuExitException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uod.gla.util;
+
+/**
+ *
+ * @author Chigozie
+ */
+public class MenuExitException extends RuntimeException {
+
+    public MenuExitException() {
+
+    }
+
+    public MenuExitException(String message) {
+        super(message);
+    }
+
+}
